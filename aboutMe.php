74a445c6e39960bbefe1412e44d7528f3e13d67b
joshcline002 <joshcline002@gmail.com>
2015-03-23 14:58:31 -0500

    Initial Site

---
+++ aboutMe.php
@@ -0,0 +1,17 @@
+<body>
+    <div>
+        <br>
+        <br>
+        <img src="Images/HooverDam.JPG" alt = "Josh Cline Picture" style="width:100%;height:300px">
+        <br>
+        <br>
+    </div>
+    <div>
+    <?php include "navBar.php" ?>
+    </div>
+    <div class = content>
+    <br>
+</body>
+<?php
+
+?>
